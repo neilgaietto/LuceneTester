61668b4740295dd0fa8719ede468702f3806dce0
Neil Gaietto <neil.gaietto@atlanticbt.com>
2016-10-18 16:44:07 -0400

    init - set up a console to test a custom tokenizer

---
+++ LuceneTester/CustomTokenizer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis;
+using Lucene.Net.Util;
+
+namespace LuceneTester
+{
+    public class CustomTokenizer : MyCharTokenizer
+    {
+        public CustomTokenizer(TextReader in_Renamed) : base(in_Renamed)
+        {
+        }
+
+        public CustomTokenizer(AttributeSource source, TextReader in_Renamed) : base(source, in_Renamed)
+        {
+        }
+
+        public CustomTokenizer(AttributeFactory factory, TextReader in_Renamed) : base(factory, in_Renamed)
+        {
+        }
+        protected override bool IsTokenChar(char c)
+        {
+            return (char.IsLetterOrDigit(c) || c == '+' || c == '#' || c == '\'' || c == '-');
+        }
+        protected override char Normalize(char c)
+        {
+            return base.Normalize(char.ToLower(c));
+        }
+    }
+}
---
+++ LuceneTester/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Analysis.Tokenattributes;
+
+namespace LuceneTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.PrintToken("Test's tested, objective-c");
+            prog.PrintToken("Test. testing");
+            prog.PrintToken("Test/test");
+            prog.PrintToken("Test-test");
+            prog.PrintToken("Test and test");
+            prog.PrintToken("Test++, c#");
+            prog.PrintToken("([Test])");
+            prog.PrintToken("l'équipe + DÉVELOPPEMENT & c#, free/form.");
+
+            Console.Read();
+        }
+
+        void PrintToken(string val)
+        {
+            Console.WriteLine($"\"{val}\"");
+            TokenStream ts = new CustomAnalyzer().TokenStream("default", new StringReader(val));
+            var termAtt = ts.GetAttribute(typeof(TermAttribute));
+
+            while (ts.IncrementToken())
+            {
+                Console.WriteLine(termAtt.ToString());
+
+            }
+            Console.WriteLine("--------------------");
+        }
+    }
+
+    public class MyAnalyzer : Analyzer
+    {
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            TokenStream stream = new WhitespaceTokenizer(reader);
+            stream = new StandardFilter(stream);
+            stream = new LowerCaseFilter(stream);
+            return new StopFilter(true, stream, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+        }
+    }
+
+    public class StandardAnalyzer : Analyzer
+    {
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            TokenStream stream = new StandardTokenizer(Lucene.Net.Util.Version.LUCENE_29, reader);
+            return stream;
+        }
+    }
+
+    public class CustomAnalyzer : Analyzer
+    {
+        public override TokenStream TokenStream(string fieldName, TextReader reader)
+        {
+            TokenStream stream = new CustomTokenizer(reader);
+            stream = new StandardFilter(stream);
+            //stream = new PorterStemFilter(stream);
+            //stream = new StopFilter(true, stream, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+            return stream;
+        }
+    }
+
+
+}
